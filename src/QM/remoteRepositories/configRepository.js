eb1c2f6cc416d478f66f9b54d6946f432956ebd3
MJ JAARA <mj_blackend@hotmail.com>
2018-04-03 12:15:47 +0300

    second commit

--- src/QM/remoteRepositories/configRepository.js
+++ src/QM/remoteRepositories/configRepository.js
@@ -3,3 +3,41 @@
 
 //for now when working on DB, we will use Getall to retrieve everything
 //later when the repository returns data from server, the server will implement getbyFilter API.
+var mssql = require("mssql");
+var config = {
+    user: "sa",
+    password: "fileworx@123",
+    server: "majd",
+    database: "new"
+};
+var connection = null;
+
+//Get All entities
+var GetAll = async function (entityname) {
+    if (connection == null)
+    {
+        connection = await mssql.connect(config);
+    }
+
+    var request= new mssql.Request();
+    var command = "select * from T_" + entityname;
+    var Results = await request.query(command);
+    return Results;
+};
+
+//Get By Filter
+var GetByFilter = async function (entityname, FilterName, FilterValue) {
+    if (connection == null)
+    {
+        connection = await mssql.connect(config);
+    }
+
+    var request= new mssql.Request();
+    var command = "select * from T_" + entityname + " where " + FilterName + " = " + FilterValue;
+    var Results = await request.query(command);
+    return Results;
+};
+
+module.exports.GetAll = GetAll;
+module.exports.GetByFilter = GetByFilter;
+
---
+++ src/QM/remoteRepositories/configRepository.spec.js
@@ -0,0 +1,18 @@
+var should = require("should");
+var mocha = require("mocha");
+var describe = mocha.describe;
+var it = mocha.it;
+var configRepository = require("./configRepository");
+
+
+describe('Start testing SQL DB', function () {
+    it('Getting Branches', async function () {
+        var result = await configRepository.GetAll("QueueBranch");
+        ((result !== undefined || result !== undefined) && result.recordset.length > 0).should.true()
+
+    });
+    it('Getting Counters',async function () {
+        var result =  await configRepository.GetAll("counter");
+        ((result !== undefined || result !== undefined) && result.recordset.length > 0).should.true()
+    });
+});
