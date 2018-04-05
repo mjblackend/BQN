7b9729d52a548b4f9d3e41146d79b387a2e0cb2e
MJ JAARA <mj_blackend@hotmail.com>
2018-04-05 16:35:47 +0300

    Generate IDs

--- src/qm/localRepositories/idGenerator.js
+++ src/qm/localRepositories/idGenerator.js
@@ -1,18 +1,47 @@
 var sqlite3 = require("./aa-sqlite");
+var CurrentSeqNumber = 0;
 
-var getNewID = async function (db) {
+//Get the ID for the first time
+var getNewIDFromDB = async function (db) {
     var that = db;
     let sql = "SELECT * FROM seq";
     var sqlRecord = await that.all(sql);
     if (sqlRecord != undefined && sqlRecord[0].seqNumber > 0) {
-        var newID = sqlRecord[0].seqNumber + 1;
-        sql = "update seq SET seqNumber = \"" + newID + "\"";
-        var isSuccessfull = await that.run(sql);
-        if (isSuccessfull) {
-            return newID;
-        }
+        return sqlRecord[0].seqNumber
     }
     return -1;
-}
+};
+
+//Update to the new ID
+var UpdateSeqOnDB = async function (db) {
+    var that = db;
+    var sql = "update seq SET seqNumber = \"" + CurrentSeqNumber + "\"";
+    var isSuccessfull = await that.run(sql);
+    if (isSuccessfull) {
+        return isSuccessfull;
+    }
+};
+
+//Generate New ID
+var getNewID = async function (db) {
+    //Get the Sequence for the first time
+    if (CurrentSeqNumber == 0) {
+        CurrentSeqNumber = await getNewIDFromDB(db);
+    }
+    //Increase the Seq with 1
+    CurrentSeqNumber = CurrentSeqNumber + 1;
+
+    //Update the Seq
+    var result = await UpdateSeqOnDB(db);
+    if (result) {
+        return CurrentSeqNumber;
+    }
+    else {
+        return -1;
+    }
+
+};
+
+
 
 module.exports.getNewID = getNewID;
