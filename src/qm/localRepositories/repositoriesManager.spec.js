2ea6bcea7934c8a860841b07a5363878624edb3d
MJ JAARA <mj_blackend@hotmail.com>
2018-04-09 17:29:46 +0300

    issue Ticket 3

--- src/qm/localRepositories/repositoriesManager.spec.js
+++ src/qm/localRepositories/repositoriesManager.spec.js
@@ -18,30 +18,30 @@ describe('Database testing', function () {
 });
 
 describe('Test Transaction Repo', function () {
-    it('Get All Transactions', async function () {
+    it('Get All Transactions successfully', async function () {
         let result = await repositoriesMgr.transactionRep.getAll();
         (result !== undefined).should.true();
     });
 
-    it('Create New transaction', async function () {
+    it('Create New transaction successfully', async function () {
         let transactioninst = new transaction();
         transactioninst.org_ID = 1;
         let result = await repositoriesMgr.transactionRep.addOrUpdate(transactioninst);
         (result == common.success).should.true();
     });
 
-    it('ADD OR UPDATE Trancaction With ID = 5', async function () {
+    it('ADD OR UPDATE Trancaction With ID = 5 successfully', async function () {
         let transactioninst = new transaction();
         transactioninst.id = 5;
         transactioninst.org_ID = 1;
         let result = await repositoriesMgr.transactionRep.addOrUpdate(transactioninst);
         (result == common.success).should.true();
     });
-    it('Get Transaction by ID, ID = 5 ', async function () {
+    it('Get Transaction by ID, ID = 5 successfully', async function () {
         let result = await repositoriesMgr.transactionRep.getFilterBy(["id"], ["5"]);
         (result !== undefined).should.true();
     });
-    it('Delete Transaction, ID = 6 ', async function () {
+    it('Delete Transaction, ID = 6 successfully', async function () {
         let transactioninst = new transaction();
         transactioninst.id = 6;
         transactioninst.org_ID = 1;
@@ -57,30 +57,30 @@ describe('Test Transaction Repo', function () {
 });
 
 describe('Test User Activity Repo', function () {
-    it('Get All user Activities', async function () {
+    it('Get All user Activities successfully', async function () {
         let result = await repositoriesMgr.userActivitiesRep.getAll();
         (result !== undefined).should.true();
     });
 
-    it('Create New Activity', async function () {
+    it('Create New Activity successfully', async function () {
         let userActivityinst = new userActivity();
         userActivityinst.org_ID = 1;
         let result = await repositoriesMgr.userActivitiesRep.addOrUpdate(userActivityinst);
         (result == common.success).should.true();
     });
 
-    it('ADD OR UPDATE Activity With ID = 5', async function () {
+    it('ADD OR UPDATE Activity With ID = 5 successfully', async function () {
         let userActivityinst = new userActivity();
         userActivityinst.id = 5;
         userActivityinst.org_ID = 1;
         let result = await repositoriesMgr.userActivitiesRep.addOrUpdate(userActivityinst);
         (result == common.success).should.true();
     });
-    it('Get Activity by ID, ID = 5 ', async function () {
+    it('Get Activity by ID, ID = 5 successfully', async function () {
         let result = await repositoriesMgr.userActivitiesRep.getFilterBy(["id"], ["5"]);
         (result !== undefined).should.true();
     });
-    it('Delete Activity, ID = 6 ', async function () {
+    it('Delete Activity, ID = 6 successfully', async function () {
         let userActivityinst = new userActivity();
         userActivityinst.id = 6;
         userActivityinst.org_ID = 1;
