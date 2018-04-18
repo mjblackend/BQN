7541a0a8dbcc202f21796b4c295fb83df4ceabf2
MJ JAARA <mj_blackend@hotmail.com>
2018-04-18 13:57:38 +0300

    Break and Open 2

--- src/common/common.js
+++ src/common/common.js
@@ -2,5 +2,5 @@ module.exports.success = 0;
 module.exports.error = -1;
 module.exports.not_valid = -2;
 module.exports.noData = -100;
-module.exports.moch=false;
+module.exports.moch=true;
 module.exports.dbConnection = "./db/queuing.db";
