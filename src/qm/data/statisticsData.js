9f1c7efba27db7395e16b7a38dbecae56291e356
MJ JAARA <mj_blackend@hotmail.com>
2018-04-23 17:52:26 +0300

    Modify UI

---
+++ src/qm/data/statisticsData.js
@@ -0,0 +1,27 @@
+class statisticsData {
+    constructor() {
+        this.ID,
+        this.QueueID,
+        this.SegmentID,
+        this.Hall_ID,
+        this.WindowID,
+        this.EmployeeID,
+        this.HallNumber,
+
+        this.WaitingCustomers,
+        this.AvgServiceTime,
+        this.AvgWaitingTime,
+        this.StatisticsDate,
+        this.ServedCustomersNo,
+        this.WaitedCustomersNo,
+        this.NoShowCustomersNo,
+        this.ASTWeight,
+        this.TotalPendingCustomersNo,
+        this.NonServedCustomersNo,
+        
+        this.SentToServer
+
+ }
+}
+module.exports = statisticsData;
+
